c6fb23c3a00098be5ff2de254e5df22774944ee6
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-13 20:02:52 -0700

    Redux outline

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { AppRegistry } from 'react-native';
 import App from './client/Components/App';
 
 //redux config
-
+//available at all times!!!
+//user
+//house
+//notification
 
 AppRegistry.registerComponent('RoommateAppMobile', () => App);

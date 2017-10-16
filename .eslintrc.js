1e631a8e613c289fd14da49869fa60892efc9e36
Philip Marazita <pmarazita@gmail.com>
2017-10-16 16:07:28 -0700

    Create signup component to begin testing aws cognito

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
-<<<<<<< HEAD
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-console": 0,
-=======
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
->>>>>>> Fix minor issues in index and linter to work properly
   }
 };

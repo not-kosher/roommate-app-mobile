90001ca381d5c0240896495dc1d00c70ddadc194
Philip Marazita <pmarazita@gmail.com>
2017-10-16 15:29:57 -0700

    Fix minor issues in index and linter to work properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
+<<<<<<< HEAD
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-console": 0,
+=======
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+>>>>>>> Fix minor issues in index and linter to work properly
   }
 };

716df511ed1bf87dbf2fa964e0187c8060e39ed2
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-16 15:17:58 -0700

    Add icons and add fonts to xcode
    This is broken

--- client/components/messageBoard/MessageBoardNav.js
+++ client/components/messageBoard/MessageBoardNav.js
@@ -1,7 +1,31 @@
+import React from 'react';
 import { TabNavigator } from 'react-navigation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Notifications from './Notifications';
 import GeneralMessages from './GeneralMessages';
+
+Notifications.navigationOptions = {
+  tabBarLabel: 'Notifications',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <MaterialCommunityIcons
+      name={focused ? 'fridge-filled' : 'fridge'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  ),
+};
+
+GeneralMessages.navigationOptions = {
+  tabBarLabel: 'Messages',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <MaterialCommunityIcons
+      name={focused ? 'message' : 'message-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  ),
+};
 
 const MessageBoardNav = TabNavigator(
   {

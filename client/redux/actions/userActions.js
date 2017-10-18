104416c4046816f3ae6932e72064d8a3720008e3
Philip Marazita <pmarazita@gmail.com>
2017-10-18 12:22:13 -0700

    Create reducers and actions for user profile properties

--- client/redux/actions/userActions.js
+++ client/redux/actions/userActions.js
@@ -9,6 +9,13 @@ export const updateUserid = (userid) => {
   // update the user id
 };
 
+export const updateUser = (user) => {
+  return {
+    type: 'UPDATE_USER',
+    user,
+  };
+};
+
 export const resetUser = () => {
   // reset user to default values when logging out
 };
--- client/redux/reducers/userReducers.js
+++ client/redux/reducers/userReducers.js
@@ -1,11 +1,19 @@
 const initialState = {
   username: '',
+  userId: 0,
+  imageUrl: '',
+  firstName: '',
+  lastName: '',
+  phone: '',
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_USERNAME':
       return { ...state, username: action.username };
+
+    case 'UPDATE_USER':
+      return { ...state, ...action.user };
 
     default: {
       return state;

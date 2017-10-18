850e8edbdf63fd10c610cff91c1b350f1393f748
Philip Marazita <pmarazita@gmail.com>
2017-10-18 12:14:43 -0700

    Outline inputs and button for editing profile

--- client/components/profile/EditProfile.js
+++ client/components/profile/EditProfile.js
@@ -1,13 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
+  Image,
   View,
   Text,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
-const EditProfile = () => (
-  <View>
-    <Text>Edit Profile</Text>
-  </View>
-);
+import { updateUsername } from '../../redux/actions/userActions';
 
-export default EditProfile;
+class EditProfile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imageUrl: this.props.imageUrl,
+      firstName: this.props.firstName,
+      lastName: this.props.lastName,
+      phone: this.props.phone,
+    };
+
+    this.saveProfile = this.saveProfile.bind(this);
+  }
+
+  saveProfile() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Edit Profile</Text>
+        <Image source={{ uri: this.state.imageUrl }} />
+        <TextInput
+          placeholder="Image URL"
+          value={this.state.imageUrl}
+          onChangeText={imageUrl => this.setState({ imageUrl })}
+        />
+        <TextInput
+          placeholder="First Name"
+          value={this.state.firstName}
+          onChangeText={firstName => this.setState({ firstName })}
+        />
+        <TextInput
+          placeholder="Last Name"
+          value={this.state.lastName}
+          onChangeText={lastName => this.setState({ lastName })}
+        />
+        <TextInput
+          placeholder="Phone Number"
+          value={this.state.phone}
+          onChangeText={phone => this.setState({ phone })}
+        />
+        <TouchableOpacity onPress={this.saveProfile} >
+          <View>
+            <Text>Save</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+} 
+
+const mapStateToProps = (store) => {
+  return {
+    username: store.user.username,
+    imageUrl: store.user.imageUrl,
+    firstName: store.user.firstName,
+    lastName: store.user.lastName,
+    phone: store.user.phone,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateUsername: (username) => {
+      dispatch(updateUsername(username));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditProfile);

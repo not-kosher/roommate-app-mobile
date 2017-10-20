9d867e235b98e8bd7fb02e2b762937b9aad5866f
Philip Marazita <pmarazita@gmail.com>
2017-10-20 11:44:12 -0700

    Add functions for creating a house in redux

--- client/redux/actions/houseActions.js
+++ client/redux/actions/houseActions.js
@@ -1,7 +1,15 @@
 import axios from '../../lib/customAxios';
 
 export const createHouse = (name) => {
-
+  return (dispatch) => {
+    axios.post('/api/houses', { name })
+      .then(({ data }) => {
+        dispatch({
+          type: 'UPDATE_HOUSE',
+          payload: data,
+        });
+      })
+  }
 };
 
 export const joinHouse = (key) => {
--- client/redux/reducers/houseReducers.js
+++ client/redux/reducers/houseReducers.js
@@ -1,5 +1,5 @@
 const initialState = {
-  id: '',
+  id: 0,
   name: '',
   key: '',
 };

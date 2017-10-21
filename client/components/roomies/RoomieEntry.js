875b5fdde70a2d8e783d66a9ce165b430fa1413b
Philip Marazita <pmarazita@gmail.com>
2017-10-20 18:44:58 -0700

    Clean up spacing

--- client/components/roomies/RoomieEntry.js
+++ client/components/roomies/RoomieEntry.js
@@ -10,11 +10,10 @@ const RoomieEntry = ({ roomie }) => {
       <Text>{`${roomie.firstName} ${roomie.lastName}`}</Text>
       <Text>{`Email: ${roomie.username}`}</Text>
       {roomie.phone && 
-          <Text>
-            {`Phone: (${roomie.phone.slice(0, 3)})-${roomie.phone.slice(3, 6)}-${roomie.phone.slice(6, 10)}`}
-          </Text>
+        <Text>
+          {`Phone: (${roomie.phone.slice(0, 3)})-${roomie.phone.slice(3, 6)}-${roomie.phone.slice(6, 10)}`}
+        </Text>
       }
-      <Text>    </Text>
     </View>
   );
 };

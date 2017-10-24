9139e37f051da93ea93e68b4c45a39a233489df4
Lillian Anderson <lilliananderson@ucla.edu>
2017-10-23 20:00:11 -0700

    add bill form adds charges, bills, and recurring bills correctly

--- client/components/financials/AddBill.js
+++ client/components/financials/AddBill.js
@@ -27,33 +27,40 @@ class AddBill extends Component {
       billName: '',
       total: '',
       success: '',
+      share: '',
+      roomieId: '',
+      abit: '',
+
     };
 
     this.submitFinancial = this.submitFinancial.bind(this);
     this.createBill = this.createBill.bind(this);
+    this.createCharges = this.createCharges.bind(this)
   }
   submitFinancial() {
     if (this.state.recurring) {
       axios.post('/api/recurringBills', {
         houseId: this.props.houseId,
         text: this.state.billName,
-        total: parseInt(this.state.total),
+        total: +(this.state.total),
         userId: this.props.userId,
         dueDate: this.state.date,
       })
         .then((result) => {
           this.createBill(result.data[0].id);
+          this.createCharges();
         })
         .catch(err => this.setState({ success: JSON.stringify(err) }));
     } else {
       this.createBill();
+      this.createCharges();
     }
   }
   createBill(recurringBill) {
     axios.post('/api/bills', {
       houseId: this.props.houseId,
       text: this.state.billName,
-      total: parseInt(this.state.total),
+      total: +(this.state.total),
       posterId: this.props.userId,
       dueDate: this.state.date,
       recurringbillId: recurringBill,
@@ -61,19 +68,37 @@ class AddBill extends Component {
       .then(() => this.setState({ success: 'sucess' }))
       .catch(err => this.setState({ success: JSON.stringify(err) }));
   }
+  createCharges() {
+    for (var roomieId in this.roomieAmmounts) {
+      axios.post('/api/charges', {
+        houseId: this.props.houseId,
+        billText: this.state.billName,
+        total: +(this.roomieAmmounts[roomieId]),
+        lenderId: this.props.userId,
+        debtorId: roomieId,
+      })
+        .then(result => this.setState({ success: JSON.stringify(result) }))
+        .catch(err => this.setState({ success: JSON.stringify(err) }));
+    };
+  }
   render() {
     return (
       <ScrollView>
-        <Text>{JSON.stringify(this.state.success)}</Text>
         <Text>Bill name:</Text>
         <TextInput
           placeholder="Bill name"
           onChangeText={name => this.setState({ billName: name })}
         />
         <Text>Total Ammmount: $</Text>
-        <TextInput 
+        <TextInput
           placeholder="Total ammount"
-          onChangeText={total => this.setState({ total: total })}
+          onChangeText={(total) => {
+            this.setState({ total: total });
+            this.setState({ share: (total / this.props.roomies.length).toFixed(2) });
+            this.props.roomies.forEach((roomie) => {
+              this.roomieAmmounts[roomie.id] = (total / this.props.roomies.length).toFixed(2);
+            });
+          }}
         />
         {this.props.roomies.map((roomie) => {
           this.state[roomie.id] = '';
@@ -81,6 +106,7 @@ class AddBill extends Component {
             <View key={roomie.id} >
               <Text>{roomie.firstName}</Text>
               <TextInput
+                defaultValue={this.state.share}
                 onChangeText={ammount => this.roomieAmmounts[roomie.id] = ammount}
               />
             </View>);
@@ -101,9 +127,6 @@ class AddBill extends Component {
         />
         <TouchableOpacity onPress={() => this.submitFinancial()}>
           <Text>SUBMIT</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.setState({ roomieAmmounts: this.roomieAmmounts })}>
-          <Text>{JSON.stringify(this.roomieAmmounts)}</Text>
         </TouchableOpacity>
       </ScrollView>
     );

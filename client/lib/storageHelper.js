9902ae18c6e133c0072ca3b91b2f53ef1d435ae1
Philip Marazita <pmarazita@gmail.com>
2017-10-26 16:27:58 -0700

    Add dependencies for fetch blob and uuid

--- client/lib/storageHelper.js
+++ client/lib/storageHelper.js
@@ -1,7 +1,13 @@
 import {
-  AWS_PROJECT_REGION,
   AWS_S3_BUCKET,
 } from 'react-native-dotenv';
+import uuid from 'react-native-uuid';
+import RNFB from 'react-native-fetch-blob';
 
 import AWS from './awsConfig';
 
+const uploadPicture = (imageUri) => {
+  const client = new AWS.S3();
+};
+
+export default uploadPicture;

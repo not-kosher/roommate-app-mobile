a59bc6742ff3a2194fc51db0eaa123c634fee9b0
Philip Marazita <pmarazita@gmail.com>
2017-10-30 19:46:43 -0700

    Add basic styling to login screens

--- client/components/login/Splash.js
+++ client/components/login/Splash.js
@@ -3,25 +3,64 @@ import {
   View,
   Text,
   TouchableOpacity,
+  Image,
 } from 'react-native';
+import {
+  Button,
+} from 'react-native-elements';
+
+import logo from '../../images/logos/landing_page.png';
+import * as color from '../../styles/common';
+
+const styles = {
+  splashContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  logoContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 112,
+    width: 325,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  buttonView: {
+    flex: 1,
+    margin: 5,
+  },
+};
 
 const Splash = props => (
-  <View>
-    <Text>LOGO!!!</Text>
-    <TouchableOpacity
-      onPress={() => props.navigation.navigate('Signup')}
-    >
-      <View>
-        <Text>Sign up</Text>
+  <View style={styles.splashContainer}>
+    <View style={styles.logoContainer}>
+      <Image
+        source={logo}
+        style={styles.logo}
+      />
+    </View>
+    <View style={styles.buttonContainer}>
+      <View style={styles.buttonView}>
+        <Button
+          large
+          title="Sign Up"
+          onPress={() => props.navigation.navigate('Signup')}
+          backgroundColor={color.PRIMARY}
+        />
       </View>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => props.navigation.navigate('Login')}
-    >
-      <View>
-        <Text>Log In</Text>
+      <View style={styles.buttonView}>
+        <Button
+          large
+          title="Log In"
+          onPress={() => props.navigation.navigate('Login')}
+          backgroundColor={color.SECONDARY}
+        />
       </View>
-    </TouchableOpacity>
+    </View>
   </View>
 );
 

24ef8cff4bb5060fcf50885cf8f278897997ba21
Philip Marazita <pmarazita@gmail.com>
2017-11-02 10:38:27 -0700

    Remove self from roomie list

--- client/components/roomies/RoomieList.js
+++ client/components/roomies/RoomieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   ScrollView,
 } from 'react-native';
@@ -8,9 +9,20 @@ import RoomieEntry from './RoomieEntry';
 const RoomieList = (props) => {
   return (
     <ScrollView>
-      {props.roomies.map(roomie => <RoomieEntry key={roomie.id} roomie={roomie} />)}
+      {props.roomies.map((roomie) => {
+        if (roomie.username !== props.username) {
+          return (<RoomieEntry key={roomie.id} roomie={roomie} />);
+        }
+        return null;
+      })}
     </ScrollView>
   );
 };
 
-export default RoomieList;
+const mapStateToProps = (store) => {
+  return {
+    username: store.user.username,
+  };
+};
+
+export default connect(mapStateToProps)(RoomieList);

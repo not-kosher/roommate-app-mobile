3d62cfc6beadd3ed4ca33768bdc19510cce5d433
Philip Marazita <pmarazita@gmail.com>
2017-11-01 18:29:36 -0700

    Add styling to drawer navigator

--- client/components/HouseNav.js
+++ client/components/HouseNav.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import { DrawerNavigator, DrawerItems } from 'react-navigation';
 import { View, StyleSheet } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+import * as color from '../styles/common';
 import MessageBoardNav from './messageBoard/MessageBoardNav';
 import FinancialsNav from './financials/FinancialsNav';
 import RoomiesNav from './roomies/RoomiesNav';
@@ -13,8 +15,21 @@ import TasksNav from './tasks/TasksNav';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: color.BG_D_GRAY,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'normal',
   },
 });
+
+const createIcon = (name, clr) => (
+  <MaterialIcons
+    name={name}
+    color={clr}
+    size={25}
+  />
+);
 
 const DrawerContent = props => (
   <View style={styles.container}>
@@ -28,13 +43,38 @@ const HouseNav = DrawerNavigator(
     MessageBoardNav: {
       screen: MessageBoardNav,
       navigationOptions: {
-        title: 'Message Board',
+        drawerLabel: 'Message Board',
+        drawerIcon: ({ tintColor }) => createIcon('message', tintColor),
       },
     },
-    FinancialsNav: { screen: FinancialsNav },
-    RoomiesNav: { screen: RoomiesNav },
-    ProfileNav: { screen: ProfileNav },
-    TasksNav: { screen: TasksNav },
+    TasksNav: {
+      screen: TasksNav,
+      navigationOptions: {
+        drawerLabel: 'Chores',
+        drawerIcon: ({ tintColor }) => createIcon('content-paste', tintColor),
+      },
+    },
+    FinancialsNav: {
+      screen: FinancialsNav,
+      navigationOptions: {
+        drawerLabel: 'Financials',
+        drawerIcon: ({ tintColor }) => createIcon('account-balance', tintColor),
+      },
+    },
+    RoomiesNav: {
+      screen: RoomiesNav,
+      navigationOptions: {
+        drawerLabel: 'Roomies',
+        drawerIcon: ({ tintColor }) => createIcon('people-outline', tintColor),
+      },
+    },
+    ProfileNav: {
+      screen: ProfileNav,
+      navigationOptions: {
+        drawerLabel: 'Profile',
+        drawerIcon: ({ tintColor }) => createIcon('account-circle', tintColor),
+      },
+    },
   },
   {
     // drawer config
@@ -42,6 +82,12 @@ const HouseNav = DrawerNavigator(
 
     // drawerWidth:
     contentComponent: DrawerContent,
+    contentOptions: {
+      activeTintColor: color.PRIMARY,
+      activeBackgroundColor: color.TEXT_M_GRAY,
+      inactiveTintColor: color.WHITE,
+      labelStyle: styles.label,
+    },
   },
 );
 

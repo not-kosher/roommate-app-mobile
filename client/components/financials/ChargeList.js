fb793cc3f57b75d7ca9d5c5e07228de1572a3dd0
Philip Marazita <pmarazita@gmail.com>
2017-11-02 20:29:47 -0700

    Render charges for everyone except self

--- client/components/financials/ChargeList.js
+++ client/components/financials/ChargeList.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   ScrollView,
 } from 'react-native';
 
 import ChargeEntry from './ChargeEntry';
 
-const ChargeList = ({ charges, deleteCharge }) => {
+const ChargeList = ({ charges, deleteCharge, username }) => {
   return (
     <ScrollView >
       {charges.map((charge, key) => {
-        return <ChargeEntry charge={charge} key={key} deleteCharge={deleteCharge} />;
+        if (charge[0].username !== username) {
+          return <ChargeEntry charge={charge} key={key} deleteCharge={deleteCharge} />;
+        }
+        return null;
       })}
     </ScrollView>
   );
 };
 
-export default ChargeList;
+const mapStateToProps = (store) => {
+  return {
+    username: store.user.username,
+  };
+};
+
+export default connect(mapStateToProps)(ChargeList);
